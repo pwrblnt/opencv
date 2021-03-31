237edf514cabe95edd5ca7b616e62f1650cb1abe
bashkirov.egormail.ru <TUJH135541tujh>
2021-03-31 21:12:51 +0300

    yo!

--- stack.py
+++ stack.py
@@ -4,6 +4,7 @@ import flask
 import telebot
 import os
 import time
+
 from flask import Flask
 
 server = Flask(__name__)
@@ -12,25 +13,29 @@ bot = telebot.TeleBot(token)
 
 print(bot.get_me())
 
+
 def log(message, answer):
     print("\n -----")
     from datetime import datetime
     print(datetime.now())
-    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name, message.from_user.last_name,
+    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
+                                                                   message.from_user.last_name,
                                                                    str(message.from_user.id), message.text))
     print(answer)
 
+
 @bot.message_handler(commands=['poh'])
 def hendle_start(message):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button1 = telebot.types.InlineKeyboardButton(text="葉隱", url="https://telegram.me/skywallker1986")
-        url_button4 = telebot.types.InlineKeyboardButton(text="Выбери бота", callback_data='qanda')
-        keyboard.row(url_button1)
-        keyboard.row(url_button4)
-        bot.send_message(message.from_user.id, 'Use this bot for success', reply_markup=keyboard)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button1 = telebot.types.InlineKeyboardButton(text="葉隱", url="https://telegram.me/skywallker1986")
+    url_button4 = telebot.types.InlineKeyboardButton(text="Выбери бота", callback_data='qanda')
+    keyboard.row(url_button1)
+    keyboard.row(url_button4)
+    bot.send_message(message.from_user.id, 'Use this bot for success', reply_markup=keyboard)
 
-        answer = "menu"
-        log(message, answer)
+    answer = "menu"
+    log(message, answer)
+
 
 @bot.message_handler(content_types=['contact'])
 def handle_text(message):
@@ -43,32 +48,39 @@ def handle_text(message):
     else:
         print('telephone_to_me')
 
+
 @bot.inline_handler(lambda query: query.query == '/yo')
 def query_text(inline_query):
     icon1 = 'https://i1.sndcdn.com/artworks-000338885529-3raizt-t500x500.jpg'
-    r = telebot.types.InlineQueryResultArticle('1', '%%%%', telebot.types.InputTextMessageContent('FLIP'), thumb_url=icon1, thumb_width=48, thumb_height=48)
-    r1 = telebot.types.InlineQueryResultAudio(2, audio_url='https://api.soundcloud.com/tracks/269416043/stream?limit=200&client_id=175c043157ffae2c6d5fed16c3d95a4c', title='test123', performer='test321')
+    r = telebot.types.InlineQueryResultArticle('1', '%%%%', telebot.types.InputTextMessageContent('FLIP'),
+                                               thumb_url=icon1, thumb_width=48, thumb_height=48)
+    r1 = telebot.types.InlineQueryResultAudio(2,
+                                              audio_url='https://api.soundcloud.com/tracks/269416043/stream?limit=200&client_id=175c043157ffae2c6d5fed16c3d95a4c',
+                                              title='test123', performer='test321')
     bot.answer_inline_query(inline_query.id, [r, r1])
+
 
 @bot.message_handler(commands=['start'])
 def hendle_start(message):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button1 = telebot.types.InlineKeyboardButton(text="Buy bot(葉隱)", callback_data='b')
-        keyboard.row(url_button1)
-        bot.send_message(message.chat.id, 'yo, ' + message.from_user.first_name + '! Send send photo', reply_markup=keyboard)
-        answer = "menu"
-        log(message, answer)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button1 = telebot.types.InlineKeyboardButton(text="Buy bot!", callback_data='b')
+    keyboard.row(url_button1)
+    bot.send_message(message.chat.id, 'Yo, ' + message.from_user.first_name + '! Send photo whit compression and enjoy')
+    answer = "menu"
+    log(message, answer)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callbacks(call):
     if call.message:
-       if call.data == 'b':
-         user_markup = telebot.types.ReplyKeyboardMarkup(True)
-         phone = telebot.types.KeyboardButton(text='電話', request_contact=True)
-         user_markup.row(phone)
-         bot.send_message(call.from_user.id, '///Send contact///', reply_markup=user_markup)
-       else:
-        print('No call')
+        if call.data == 'b':
+            user_markup = telebot.types.ReplyKeyboardMarkup(True)
+            phone = telebot.types.KeyboardButton(text='電話', request_contact=True)
+            user_markup.row(phone)
+            bot.send_message(call.from_user.id, 'Click button - 電話', reply_markup=user_markup)
+        else:
+            print('No call')
+
 
 @bot.message_handler(content_types=['photo'])
 def handle_text(message):
@@ -82,25 +94,14 @@ def handle_text(message):
         os.system('python3 main.py')
         img = open('out/0000.jpg', 'rb')
         bot.send_photo(message.chat.id, photo=img)
-
-
-
     else:
         print('photoPixel')
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text == "йо":
         answer = "йо"
-        log(message, answer)
-        user_markup = telebot.types.ReplyKeyboardMarkup(False)
-        test1 = telebot.types.KeyboardButton(text='Нет')
-        test2 = telebot.types.KeyboardButton(text='Да')
-        user_markup.row(test1)
-        user_markup.row(test2)
-        bot.send_message(message.chat.id,
-                         'йо, ' + message.from_user.first_name + ' мб ты пидр?',
-                         reply_markup=user_markup)
         log(message, answer)
     else:
         bot.send_message(message.chat.id, 'You send scam, you potentially lame')
