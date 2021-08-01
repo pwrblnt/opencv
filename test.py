e361b4f196bbc316a51cd5016076a12a74152eac
bashkirov.egormail.ru <TUJH135541tujh>
2021-08-01 18:56:51 +0300

    new

--- test.py
+++ test.py
@@ -1,16 +1,40 @@
 import main
 
 import telebot
-token = "788811717:AAEoeDB2eFrSrTtxSGXusZh4cQZ7IoPBPTQ"
+import time
+token = "788811717:AAFCXXumZJkwkJFy_PRkRUZQe8jww_2RlMo"
 bot = telebot.TeleBot(token)
 
+print(bot.get_me())
 
-def test():
-    app = main.ArtConverter(str(33), 10)
+
+def log(message, answer):
+    print("\n -----")
+    from datetime import datetime
+    print(datetime.now())
+    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
+                                                                   message.from_user.last_name,
+                                                                   str(message.from_user.id), message.text))
+    print(answer)
+
+
+@bot.message_handler(commands=['start'])
+def hendle_start(message):
+    app = main.ArtConverter(str(444), 3)
     app.run()
-    img = open(f'out/' + '59290719' + '.jpg', 'rb')
+    img = open(f'out/' + '444' + '.jpg', 'rb')
+    bot.send_message(59290719, 'ewfef')
     bot.send_photo(59290719, photo=img)
+    answer = "menu"
+    log(message, answer)
 
 
-test()
+while True:
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)
+        # или import traceback; traceback.print_exc() для печати полной инфы
+        time.sleep(15)
 
